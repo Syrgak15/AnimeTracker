dec46d1e410840a7f1a72b86d1564212ae1e456a
Syrgak Sulaimanov <delcatessen@gmail.com>
2026-04-07 17:22:47 +0600

    changed header component's functionality

--- components/HeaderComponents/Header/Header.tsx
+++ components/HeaderComponents/Header/Header.tsx
@@ -5,46 +5,24 @@ import './style.css'
 import useScrollDirection from '@/hooks/useScrollDirection/useScrollDirection';
 import SignInButton from "@/components/HeaderComponents/SignInButton/SignInButton";
 import AudioPlayer from "@/components/HeaderComponents/AudioPlayer/AudioPlayer";
-import {useEffect, useState} from "react";
 import Drawer from '@/components/MUI-components/Drawer/Drawer';
 
 export default function Header() {
     const scrollDirection = useScrollDirection();
-    const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth,
-    });
-
     const headerLinks = [
         { name: 'Producers', href: '/producers' },
     ]
-
-    useEffect(() => {
-        const handleResize = () => {
-            if(window.innerWidth !== windowSize.width) {
-                setWindowSize({
-                    width: window.innerWidth,
-                });
-            }
-        }
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
-        });
-
     return (
         <div className={`header scroll-${scrollDirection}`}>
             <div className="header-links">
-                {windowSize.width > 768 ? (
-                    headerLinks.map(link => (
-                        <Link className="header-link" href={link.href} key={link.name}>
-                            {link.name}
-                        </Link>
-                    ))
-                ) : (
+                <div className="header-link header-links-desktop">
+                    {headerLinks.map(link => (
+                        <Link style={{textDecoration: "none", color: "inherit"}} href={link.href} key={link.name}>{link.name}</Link>
+                    ))}
+                </div>
+                <div className="header-link header-links-mobile">
                     <Drawer headerLinks={headerLinks}/>
-                )}
+                </div>
             </div>
 
             <div className="header-center">
